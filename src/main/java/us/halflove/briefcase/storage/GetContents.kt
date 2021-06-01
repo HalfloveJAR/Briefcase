86e3e40a42e76bebc733abe769d0557c75831353
BuildTools <unconfigured@null.spigotmc.org>
2021-06-01 15:58:25 -0600

    Initial commit

---
+++ src/main/java/us/halflove/briefcase/storage/GetContents.kt
@@ -0,0 +1,39 @@
+package us.halflove.briefcase.storage
+
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+/*
+*
+* @author Halflove
+*
+* https://Halflove.us
+* https://github.com/HalfloveJAR
+*
+*/
+
+object GetContents {
+
+    fun getBriefcaseItemStack(slot: Int): ItemStack{
+
+        var selectedItem: ItemStack = ItemStack(
+            Material.valueOf(BriefcaseStorage.data.get("contents.$slot.type").toString()),
+            BriefcaseStorage.data.get("contents.$slot.amount") as Int
+        )
+
+        if(selectedItem.type == Material.WRITTEN_BOOK){
+
+        }
+
+        if(BriefcaseStorage.data.get("contents.$slot.enchants") != "{}"){
+            //set enchants
+        }
+
+        if(BriefcaseStorage.data.get("contents.$slot.name") != ""){
+            //set display name
+        }
+
+        return selectedItem
+    }
+
+}
